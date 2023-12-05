ea99f34aecb79e99ca576b5e114d74b4df55ad79
deboschr <debospencher@gmail.com>
2023-12-05 17:48:47 +0700

    merubah nama databse dari tubes-kamin menjadi tubes_kamin

--- config/connection_database.js
+++ config/connection_database.js
@@ -1,6 +1,6 @@
 import { Sequelize } from "sequelize";
 
-const sequelize = new Sequelize("tubes-kamin", "root", "", {
+const sequelize = new Sequelize("tubes_kamin", "root", "", {
 	host: "localhost",
 	dialect: "mysql",
 });
