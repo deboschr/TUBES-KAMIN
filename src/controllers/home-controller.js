4b42ab645f4effbc04703ff3abef64a33d5584cb
deboschr <debospencher@gmail.com>
2023-12-07 11:37:56 +0700

    membuat test digital signature

--- src/controllers/home-controller.js
+++ src/controllers/home-controller.js
@@ -1,9 +1,4 @@
-import { key_generate } from "../utils/generate-key-util.js";
 export const home_page = (req, res) => {
-	const key = key_generate();
-
-	res.send(key);
-
 	res.render("home-page", {
 		title: "Digital Signature",
 		layout: "layouts/main",
--- src/utils/generate-key-util.js
+++ src/utils/generate-key-util.js
@@ -5,13 +5,37 @@ export const key_generate = () => {
 		modulusLength: 4096,
 		publicKeyEncoding: {
 			type: "spki",
-			format: "der",
+			format: "pem",
 		},
 		privateKeyEncoding: {
 			type: "pkcs8",
-			format: "der",
+			format: "pem",
+			cipher: "aes-256-cbc",
+			passphrase: "top secret",
 		},
 	});
 
 	return { publicKey, privateKey };
+};
+
+export const signing = () => {
+	try {
+		const { publicKey, privateKey } = crypto.generateKeyPairSync("rsa", {
+			modulusLength: 4096,
+			publicKeyEncoding: {
+				type: "spki",
+				format: "pem",
+			},
+			privateKeyEncoding: {
+				type: "pkcs8",
+				format: "pem",
+				cipher: "aes-256-cbc",
+				passphrase: "top secret",
+			},
+		});
+
+		return { publicKey, privateKey };
+	} catch (error) {
+		console.log(error);
+	}
 };
